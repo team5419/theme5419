b49c9c3018475e65b066a4ecc514212d0a23b21f
FelixMo42 <felix@tacocat.com>
2019-12-22 17:18:58 -0800

    phpified style

--- functions.php
+++ functions.php
@@ -1,7 +1,13 @@
 <?php
-    wp_enqueue_style( ‘style’, get_stylesheet_uri() );
+    wp_enqueue_style( 'base stlye', get_template_directory_uri() . '/assets/css/main.css');
 
     // add fonts
-    wp_enqueue_style( ‘style’, "https://fonts.googleapis.com/css?family=Righteous|Comfortaa&display=swap" );
-    wp_enqueue_style( ‘style’, "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" );
+    wp_enqueue_style(
+        "Righteous and Comfortaa fonts",
+        "https://fonts.googleapis.com/css?family=Righteous|Comfortaa&display=swap"
+    );
+    wp_enqueue_style(
+        "font awesome",
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    );
 ?>
