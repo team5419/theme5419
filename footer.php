0554465e7ad302ba8eefc442f2ef40ef7836b316
FelixMo42 <felix@tacocat.com>
2020-05-28 09:56:12 -0700

    minor fixes

--- footer.php
+++ footer.php
@@ -12,9 +12,9 @@
 ?>
 
 		<footer class="theme5419-footer">
-			<div class="theme5419-left"></div><div class="theme5419-content">
+			<div class="theme5419-left"></div>
 			<div class="theme5419-content"></div>
-			</div><div class="theme5419-right"></div>
+			<div class="theme5419-right"></div>
 		</footer>
 
 		<?php wp_footer(); ?>
