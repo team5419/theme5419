4b48b987c74d01981f81f5531f7dcc7faebf7917
FelixMo42 <felix@tacocat.com>
2020-05-28 09:24:42 -0700

    readded footer

--- footer.php
+++ footer.php
@@ -11,24 +11,14 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">Automattic</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		</div>
 
-<?php wp_footer(); ?>
+		<footer class="theme5419-footer">
+			<div class="theme5419-left"></div>
+			<div class="theme5419-content"></div>
+			<div class="theme5419-right"></div>
+		</footer>
 
-</body>
+		<?php wp_footer(); ?>
+	</body>
 </html>
