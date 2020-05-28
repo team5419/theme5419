05b88051522b5b23c9369622dd64661e1cb28c79
FelixMo42 <felix@tacocat.com>
2020-05-28 08:27:18 -0700

    added content

--- index.php
+++ index.php
@@ -1,18 +1,56 @@
-<?php get_header(); ?>
+<?php
+    /**
+     * The main template file
+     *
+     * This is the most generic template file in a WordPress theme
+     * and one of the two required files for a theme (the other being style.css).
+     * It is used to display a page when nothing more specific matches a query.
+     * E.g., it puts together the home page when no home.php file exists.
+     *
+     * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+     *
+     * @package _s
+     */
 
-<?php 
-    if ( have_posts() ) {
-        while ( have_posts() ) { 
-            the_post(); 
-            // Include the page content
-            get_template_part( 'content', 'page' );   	
-
-            // If comments are open or the page has at least one comment, load the comments template.
-            if ( comments_open() || get_comments_number() ) {
-                comments_template();
-            }
-        } 
-    }
+    get_header();
 ?>
 
-<?php get_footer(); ?>
+	<main id="primary" class="site-main">
+
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) :
+				?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) {
+				the_post();
+
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+            }
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
--- page.php
+++ page.php
@@ -1,21 +1,37 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package _s
+ */
 
-<?php 
-    // Start the Loop
-    if ( have_posts() ) { 
-        while ( have_posts() ) { 
-            the_post(); 
-            // Include the page content
-            get_template_part( 'content', 'page' );   	
-
-            // If comments are open or the page has at least one comment, load the comments template.
-            if ( comments_open() || get_comments_number() ) {
-                comments_template();
-            }
-            
-        // End the Loop
-        } 
-    }
+get_header();
 ?>
 
-<?php get_footer(); ?>
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
