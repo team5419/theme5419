d273a761045b39cde908fe00548fe03bbe869a03
FelixMo42 <felix@tacocat.com>
2020-05-28 08:43:09 -0700

    updated theme names

--- page.php
+++ page.php
@@ -9,7 +9,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package _s
+ * @package theme5419
  */
 
 get_header();
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -4,7 +4,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package _s
+ * @package theme5419
  */
 
 ?>
@@ -22,7 +22,7 @@
 
 		wp_link_pages(
 			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'theme5419' ),
 				'after'  => '</div>',
 			)
 		);
@@ -36,7 +36,7 @@
 				sprintf(
 					wp_kses(
 						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', '_s' ),
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'theme5419' ),
 						array(
 							'span' => array(
 								'class' => array(),
