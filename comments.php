a3576cb5783d82019aa2c0684fe3a72df10328f7
lemmaammel <67166525+lemmaammel@users.noreply.github.com>
2021-05-28 16:16:12 -0500

    test

--- comments.php
+++ comments.php
@@ -26,14 +26,14 @@ if () {
 <div id="comments" class="comments-area">
 
 	<?php
-	/*
+	
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) :
-	*/
+	
 		?>
 		<h2 class="comments-title">
 			<?php
-			/*
+			
 			$_s_comment_count = get_comments_number();
 			if ( '1' === $_s_comment_count ) {
 				printf(
@@ -49,7 +49,7 @@ if () {
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
 			}
-			*/
+			
 			?>
 		</h2><!-- .comments-title -->
 
@@ -57,19 +57,19 @@ if () {
 		
 		<ol class="comment-list">
 			<?php
-			/*
+			
 			wp_list_comments(
 				array(
 					'style'      => 'ol',
 					'short_ping' => true,
 				)
 			);
-			*/
+			
 			?>
 		</ol><!-- .comment-list -->
 
 		<?php
-		/*
+		
 		the_comments_navigation();
 
 		// If comments are closed and there are comments, let's leave a little note, shall we?
